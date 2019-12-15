2bf4e1e36612398f511e8b4bf229e86b88393af9
amihaescu <andrei.mihaescu91@gmail.com>
2019-12-15 13:57:16 +0200

    - added update product endpoint

--- src/main/java/ro/amihaescu/stamford/web/controller/ProductController.java
+++ src/main/java/ro/amihaescu/stamford/web/controller/ProductController.java
@@ -2,13 +2,16 @@ package ro.amihaescu.stamford.web.controller;
 
 import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ro.amihaescu.stamford.model.Product;
 import ro.amihaescu.stamford.repository.ProductRepository;
 import ro.amihaescu.stamford.web.dto.ProductDTO;
+import ro.amihaescu.stamford.web.exception.ProductNotFoundException;
 import ro.amihaescu.stamford.web.mapper.ProductMapper;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("product")
@@ -27,6 +30,21 @@ public class ProductController {
     public ResponseEntity createProduct(@RequestBody ProductDTO productDTO) {
         log.info("Creating product");
         productRepository.save(productMapper.toEntity(productDTO));
-        return  ResponseEntity.ok().build();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> allProducts = productRepository.findAll().stream().map(productMapper::toDTO).collect(Collectors.toList());
+        return ResponseEntity.ok().body(allProducts);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody ProductDTO updatedProduct) throws ProductNotFoundException {
+        productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id.toString()));
+        Product product = productMapper.toEntity(updatedProduct);
+        product.setId(id);
+        productRepository.save(product);
+        return ResponseEntity.ok(product);
     }
 }
--- src/test/java/ro/amihaescu/stamford/web/controller/ProductControllerTest.java
+++ src/test/java/ro/amihaescu/stamford/web/controller/ProductControllerTest.java
@@ -9,12 +9,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import ro.amihaescu.stamford.model.Product;
 import ro.amihaescu.stamford.repository.ProductRepository;
 import ro.amihaescu.stamford.web.dto.ProductDTO;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -40,13 +46,57 @@ public class ProductControllerTest {
     }
 
     @Test
-    public void createProduct() throws Exception {
+    public void successfullyCreateProduct() throws Exception {
         mockMvc.perform(post("/product")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(objectMapper.writeValueAsString(ProductDTO.builder().name("Beer").price(100.00).build())))
                 .andExpect(status().isOk());
 
         assertEquals(1, productRepository.findAll().size());
+    }
+
+    @Test
+    public void successfullyGetAllProducts() throws Exception {
+        productRepository.saveAll(Arrays.asList(
+                Product.builder().name("Coke").price(5.00).build(),
+                Product.builder().name("Water").price(4.50).build()
+        ));
+
+        mockMvc.perform(get("/product"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$.[0].name").isString())
+                .andExpect(jsonPath("$.[0].price").isNumber());
+    }
+
+    @Test
+    public void successfullyUpdate_ExistingProduct() throws Exception {
+        Product savedProduct = productRepository.save(Product.builder().price(5.00).name("Coca Cola").build());
+        ProductDTO updatedProduct = ProductDTO.builder().name("Coca Cola Light").price(5.00).build();
+
+        mockMvc.perform(
+                put("/product/{id}", savedProduct.getId())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(updatedProduct)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Coca Cola Light"))
+                .andExpect(jsonPath("$.price").value(5.00));
+    }
+
+    @Test
+    public void failToUpdate_ProductNotFound() throws Exception {
+        ProductDTO updatedProduct = ProductDTO.builder().name("Coca Cola Light").price(5.00).build();
+
+        MvcResult mvcResult = mockMvc.perform(
+                put("/product/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(updatedProduct)))
+                .andExpect(status().isNotFound())
+                .andReturn();
+        
+        assertNotNull(mvcResult.getResolvedException());
+        assertEquals("Product with id 1 not found", mvcResult.getResolvedException().getMessage());
 
     }
 
